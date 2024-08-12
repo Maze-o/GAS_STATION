55f132a57b38cee4edc91534ebe3a7390e68cc01
Maze-o <ilsame9970@naver.com>
2024-08-12 14:17:20 +0900

    GIT CONNECT

---
+++ src/test/java/com/project/gas/GasApplicationTests.java
@@ -0,0 +1,14 @@
+package com.project.gas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
