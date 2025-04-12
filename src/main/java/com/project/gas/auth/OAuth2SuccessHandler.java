33373b31cdf9414f026fb68cc79482dbab5768ac
Maze-o <ilsame9970@naver.com>
2025-04-12 18:26:17 +0900

    .

--- src/main/java/com/project/gas/auth/OAuth2SuccessHandler.java
+++ src/main/java/com/project/gas/auth/OAuth2SuccessHandler.java
@@ -58,10 +58,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         }
         
         User user = new User();
-<<<<<<< HEAD
         user.setProvider(provider);
-=======
->>>>>>> f04dd3bd4d3b0616cb0c2f7e3468a92cad34002f
         user.setUsername(username);
         user.setUserid(null);
         
